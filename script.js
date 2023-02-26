97ee68aba7bee3c79f7734c4704484d17f3e974f
varatharajagokul <125578341+varatharajagokul@users.noreply.github.com>
2023-02-26 15:51:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+//console.log("hello world")
+
+/*const array=[23,34,56,78,96]
+console.log(array.indexOf(56))
+console.log(array.indexOf(96))
+
+console.log(array.length)*/
+
+/*const students ={
+    name:"student1",
+    age:23,
+    place:"chennai"
+    }
+
+console.log(students.name)
+console.log(students)
+console.log(students.age)*/
+
+const arrayofObject=[
+    {
+    name:"student1",
+    age:23,
+    place:"chennai"
+    },
+    {
+    name:"student2",
+    age:26,
+    place:"chennai"
+    },
+    {
+    name:"student3",
+    age:27,
+    place:"chennai"
+    }
+]
+console.log(arrayofObject)
+
+/*var a = 5;
+var b = 6;
+console.log(a+b)*/
+
+const array = [1,2,3,4,5,6]
+console.log(array*2)
+
+/*const array =[23,4,6,45,67]
+array.push(55)
+
+console.log(array)*/
+
+/*const array =[23,4,6,45,67]
+array.pop(67)
+
+console.log(array)*/
+
+/*var a = "hello";
+var b = "world";
+console.log(a+b)*/
+
+/*a=6
+b=2
+console.log(a*b)*/
+
+/*var string = [23,4,6,45,67]
+string.reverse()
+console.log(string)*/
+
+/*var a = 2
+console.log(a*1000)
+console.log(a*100000)*/
